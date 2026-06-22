12ad799f91f756217e781292adf0c0066c2593e5
isrraely <raely.crtz1011@gmail.com>
2026-06-21 22:32:19 -0300

    updates.

--- backend/src/routes/create-trip.ts
+++ backend/src/routes/create-trip.ts
@@ -15,10 +15,11 @@ export async function createTrip(app: FastifyInstance) {
                 ends_at: z.coerce.date(),
                 owner_name: z.string(),
                 owner_email: z.string().email(),
+                emails_to_invite: z.array(z.string().email()),
             })
         }
     }, async (request) => {
-        const { destination, starts_at, ends_at, owner_name, owner_email } = request.body
+        const { destination, starts_at, ends_at, owner_name, owner_email, emails_to_invite } = request.body
 
         if (dayjs(starts_at).isBefore(new Date())) {
             throw new Error("Start date cannot be in the past");
@@ -28,13 +29,35 @@ export async function createTrip(app: FastifyInstance) {
             throw new Error("End date cannot be before start date");
         }
 
+
+
         const trip = await prisma.trip.create({
             data: {
                 destination,
                 starts_at,
                 ends_at,
+                Participant: {
+                    createMany: {
+                        data: [
+                            {
+                                name: owner_name,
+                                email: owner_email,
+                                is_owner: true,
+                                is_confirmed: true,
+                            },
+                            ...emails_to_invite.map(email => ({
+                                email,
+                                is_owner: false,
+                                is_confirmed: false,
+                            }))
+                        ]
+                    }
+                }
             }
         })
+
+        const formattedStartDate = dayjs(starts_at).format('DD/MM/YYYY')
+
         const mail = await getMailClient()
 
         const message = await mail.sendMail({
@@ -46,8 +69,21 @@ export async function createTrip(app: FastifyInstance) {
                 name: owner_name,
                 address: owner_email,
             },
-            subject: 'Testando envio de e-mail',
-            html: `<p> Teste do envio de e-mail </p>`
+            subject: `Confirme sua viagem para ${destination}`,
+            html: `
+            
+            <div style="font-family: sans-serif; font-size: 16px; line-height: 1.6;">
+                <p>Você solicitou a criação de uma viagem para <strong>${destination}</strong>, Brasil nas datas de <strong>16 a 27 de Agosto de 2024</strong>.</p>
+                <p></p>
+                <p>Para confirmar sua viagem, clique no link abaixo:</p>
+                <p></p>
+                <p><a href="">Confirmar viagem</a></p>
+                <p></p>
+                <p>Caso esteja usando o dispositivo móvel, você também pode confirmar a criação da viagem pelos aplicativos:</p>
+                <p></p>
+                <p>Caso você não saiba do que se trata esse e-mail, apenas ignore esse e-mail.</p>
+            </div>
+            `.trim(),
         })
 
         console.log(nodemailer.getTestMessageUrl(message))
